78fb1d5d2308a91ca71453f3723acf5aa58c4cb9
Dipesh Arora <dipesharora@msn.com>
2022-09-25 13:57:08 -0400

    Update module to fix address prefixes

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@
 ## Create Subnet
 #########################################################
 resource "azurerm_subnet" "subnet" {
-  resource_group_name       = var.resource_group_name
-  virtual_network_name      = var.vnet_name
-  name                      = var.subnet_name
-  address_prefix            = var.subnet_address_prefix
-  service_endpoints         = var.subnet_service_endpoints
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.vnet_name
+  name                 = var.subnet_name
+  address_prefixes     = var.subnet_address_prefixes
+  service_endpoints    = var.subnet_service_endpoints
 }
--- variables.tf
+++ variables.tf
@@ -4,27 +4,27 @@
 
 # Resource Group
 variable "resource_group_name" {
-    type        =   string
-    description =   "Set this to the name of the Resource Group in which Virtual Network is created."
+  type        = string
+  description = "Set this to the name of the Resource Group in which Virtual Network is created."
 }
 
 # VNET
 variable "vnet_name" {
-    type        =   string
-    description =   "Set this to the name of the Virtual Network."
+  type        = string
+  description = "Set this to the name of the Virtual Network."
 }
 
 # Subnets
 variable "subnet_name" {
-    type        =   string
-    description =   "Set this to the name of the Subnet to be created."
+  type        = string
+  description = "Set this to the name of the Subnet to be created."
 }
-variable "subnet_address_prefix" {
-    type        =   string
-    description =   "Set this to the address prefix to use for the Subnet."
+variable "subnet_address_prefixes" {
+  type        = list(any)
+  description = "Set this to the list of address prefixes to use for the Subnet."
 }
 variable "subnet_service_endpoints" {
-    type        =   list
-    description =   "Set this to the list of Service endpoints to associate with the subnet."
-    default =   []
+  type        = list(any)
+  description = "Set this to the list of Service endpoints to associate with the subnet."
+  default     = []
 }
